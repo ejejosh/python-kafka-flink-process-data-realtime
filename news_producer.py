f2fb3af917f96a1c15c0c6700667a09a1a22ff1e
Eje Joshua <eje_joshua@yahoo.com>
2024-09-11 18:03:11 +0100

    Fixed producer connection issue

--- news_producer.py
+++ news_producer.py
@@ -10,14 +10,21 @@ from alpaca_config.keys import config
 
 def get_producer(brokers: List[str]):
     producer = KafkaProducer(
-        bootstrap_servers=brokers,
-        key_serializer=str.encode,
-        value_serializer=lambda v: json.dumps(v).encode('utf-8')
+        bootstrap_servers = brokers,
+        key_serializer = str.encode,
+        value_serializer = lambda v: json.dumps(v).encode('utf-8'),
+        api_version = (2, 0, 2)
     )
     return producer
 
 
-def produce_historical_news(redpanda_client: KafkaProducer, start_date: str, end_date: str, symbols: List[str], topic: str):
+def produce_historical_news(
+        redpanda_client: KafkaProducer,
+        start_date: str,
+        end_date: str,
+        symbols: List[str],
+        topic: str
+    ):
     key_id = config['key_id']
     secret_key = config['secret_key']
     base_url = config['base_url']
@@ -30,16 +37,17 @@ def produce_historical_news(redpanda_client: KafkaProducer, start_date: str, end
             start=start_date,
             end=end_date,
             limit=5,
-            sort=Sort.ACS,
-            include_content=False
+            sort=Sort.ASC,
+            include_content=False,
         )
 
+        print(news)
 
 if __name__ == "__main__":
     produce_historical_news(
         get_producer(config['redpanda_brokers']),
         topic='market-news',
         start_date='2024-01-01',
-        end_date='2924-09-10',
+        end_date='2024-09-10',
         symbols=['AAPL', 'Apple']
     )
