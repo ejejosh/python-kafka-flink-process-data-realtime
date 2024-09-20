9e85aeb42948f3f9063f4a310c5be1da11cadef9
Eje Joshua <eje_joshua@yahoo.com>
2024-09-20 15:28:56 +0100

    Fixed producer/kafka conn issue

--- news_producer.py
+++ news_producer.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import List
 from kafka import KafkaProducer
 import json
@@ -6,6 +7,8 @@ from alpaca_trade_api.common import URL
 from alpaca.common import Sort
 
 from alpaca_config.keys import config
+
+from utils import get_sentiment
 
 
 def get_producer(brokers: List[str]):
@@ -41,13 +44,40 @@ def produce_historical_news(
             include_content=False,
         )
 
-        print(news)
+        for i, row in enumerate (news):
+            article = row._raw
+            should_proceed = any(term in article['headline'] for term in symbols)
+            if not should_proceed:
+                continue
+
+            timestamp_ms = int(row.created_at.timestamp() * 1600)
+            timestamp = datetime.fromtimestamp(row.created_at.timestamp())
+            article['timestamp'] = timestamp.strftime('&Y-%m-%d %H:%M:*S')
+            article['timestamp_ms'] = timestamp_ms
+            article['data_provider'] = 'alpaca'
+            article['sentiment'] = get_sentiment(article['headline'])
+            article.pop('symbols')
+            article['symbol'] = symbol
+
+            try:
+                future = redpanda_client.send(
+                    topic=topic,
+                    key=symbol,
+                    value=article,
+                    timestamp_ms=timestamp_ms
+                )
+                _ = future.get(timeout=10)
+                print(f'Sent {i+1} articles to {topic}')
+            except Exception as e:
+                print(f'Failed to send article: {article}')
+                print(e)
+
 
 if __name__ == "__main__":
     produce_historical_news(
         get_producer(config['redpanda_brokers']),
         topic='market-news',
         start_date='2024-01-01',
-        end_date='2024-09-10',
+        end_date='2024-09-20',
         symbols=['AAPL', 'Apple']
     )
