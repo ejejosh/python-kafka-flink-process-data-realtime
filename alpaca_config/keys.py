9c3b1535ef381fd9c67cd05b16b195592ff545f4
Joshua Eje <88008351+ejejosh@users.noreply.github.com>
2024-09-20 19:22:08 +0100

    Update keys.py

--- alpaca_config/keys.py
+++ alpaca_config/keys.py
@@ -1,6 +1,6 @@
 config = {
-    'key_id': '<YOUR-ALPACA-KEY-ID>',
-    'secret_key': '<YOUR-ALPACA-API-SECRETE-KEY>',
+    'key_id': 'ALPACA-KEY-ID',
+    'secret_key': 'ALPACA-API-SECRETE-KEY',
     'redpanda_brokers': 'localhost:19092, localhost:29092, localhost:39092',
     'base_url': 'https://data.alpaca.markets/v2/',
     'trade_api_base_url': 'https://paper-api.alpaca.markets/v2'
